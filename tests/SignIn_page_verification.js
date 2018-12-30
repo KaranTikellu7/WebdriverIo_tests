970c57764b5f232d066cdc4db6c522274bad41cf
KaranTikellu7 <tikellukaran@gmail.com>
2018-12-30 23:42:07 +0530

    Corrected the indentations of tests and pageobject files

--- tests/SignIn_page_verification.js
+++ tests/SignIn_page_verification.js
@@ -1,34 +1,32 @@
 const signin = require('../PageObjects/SignIn.Page')
 
-beforeEach(() => {
+beforeEach(() => 
+{
     browser.url('./')
 })
 
-describe('Verifying Elements on SignIn page', () => {
-
-    it('Verifing Headers', () => {
-
-        expect(signin.HeaderLabel.getText()).is.equal("AceInvoice")
+describe('Verifying Elements on SignIn page', () => 
+{
+    it('Verifing Headers', () => 
+    {
+        expect(signin.Header_Label.getText()).is.equal("AceInvoice")
         expect(signin.TagLine.getText()).is.equal("Time Tracking and Invoicing")
         expect(signin.Aceinvoice_Logo.isExisting()).to.true
-
     })
 
-    it('Verifing LoginBox', () => {
-
+    it('Verifing LoginBox', () => 
+    {
         expect(signin.SignIn_Label.isExisting()).to.true
-        expect(signin.EmailTextbox.isExisting()).to.true
-        expect(signin.PasswordTextbox.isExisting()).to.true
+        expect(signin.Email_Textbox.isExisting()).to.true
+        expect(signin.Password_Textbox.isExisting()).to.true
         expect(signin.RememberMe_Label.isExisting()).to.true
-        
-        expect(signin.LoginButton.getValue()).to.be.equal("Login")
-        expect(signin.LoginButton.isExisting()).to.true
-
+        expect(signin.Login_Button.getValue()).to.be.equal("Login")
+        expect(signin.Login_Button.isExisting()).to.true
         expect(signin.ForgetPassword_Link.isExisting()).to.true
-
     })
 
-    it('Verifying signin Block', () => {
+    it('Verifying signin Block', () => 
+    {
         expect(signin.Dont_have_account_Link.isExisting()).to.true
     })
 })
