4d393575c74dfeb9cde33f4a8e407a01cf88afa0
KaranTikellu7 <tikellukaran@gmail.com>
2018-12-31 12:48:09 +0530

    Modified test file from isExisting to isVisible and corrected the chains in assertion

--- tests/SignIn_page_verification.js
+++ tests/SignIn_page_verification.js
@@ -9,25 +9,25 @@ describe('Verifying Elements on SignIn page', () =>
 {
     it('Verifing Headers', () => 
     {
-        expect(signin.Header_Label.getText()).is.equal("AceInvoice")
-        expect(signin.TagLine.getText()).is.equal("Time Tracking and Invoicing")
-        expect(signin.Aceinvoice_Logo.isExisting()).to.true
+        expect(signin.Header_Label.getText()).to.equal("AceInvoice")
+        expect(signin.TagLine.getText()).to.equal("Time Tracking and Invoicing")
+        expect(signin.Aceinvoice_Logo.isVisible()).to.true
     })
 
     it('Verifing LoginBox', () => 
     {
-        expect(signin.SignIn_Label.isExisting()).to.true
-        expect(signin.Email_Textbox.isExisting()).to.true
-        expect(signin.Password_Textbox.isExisting()).to.true
-        expect(signin.RememberMe_Label.isExisting()).to.true
-        expect(signin.RememberMe_Label.getText()).is.equal("Remember Me")
-        expect(signin.Login_Button.getValue()).to.be.equal("Login")
-        expect(signin.Login_Button.isExisting()).to.true
-        expect(signin.ForgetPassword_Link.isExisting()).to.true
+        expect(signin.SignIn_Label.isVisible()).to.true
+        expect(signin.Email_Textbox.isVisible()).to.true
+        expect(signin.Password_Textbox.isVisible()).to.true
+        expect(signin.RememberMe_Label.isVisible()).to.true
+        expect(signin.RememberMe_Label.getText()).to.equal("Remember Me")
+        expect(signin.Login_Button.getValue()).to.equal("Login")
+        expect(signin.Login_Button.isVisible()).to.true
+        expect(signin.ForgetPassword_Link.isVisible()).to.true
     })
 
     it('Verifying SignUp Block', () => 
     {
-        expect(signin.Dont_have_account_Link.isExisting()).to.true
+        expect(signin.Dont_have_account_Link.isVisible()).to.true
     })
 })
